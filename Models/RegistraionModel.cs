a868a879521df01ba65103aa7ece6076021559f5
ComputerUser158 <oneringraw@gmail.com>
2021-03-22 23:33:37 -0500

    Add project files.

---
+++ Models/RegistraionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddValidation.Models
+{
+    public class RegistraionModel
+    {
+
+    }
+}
